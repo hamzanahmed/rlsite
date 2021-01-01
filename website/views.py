6e9229c127c8ff2831c18aa826f42c1d03b5bbb1
Hamza Ahmed <hamzanahmed@gmail.com>
2021-01-01 12:14:23 +0500

    initial commit

---
+++ website/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Contact
+from django.http import HttpResponse
+
+def home(request):
+    if request.method=="POST":
+        contact=Contact()
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        message=request.POST.get('message')
+        contact.name=name
+        contact.email=email
+        contact.message=message
+        contact.save()
+        return render(request, 'home.html', {})
+
+    return render(request, 'home.html', {})
+
+
+# Create your views here.
