70101309e03293576f4242a2bf2478ac7bd3540f
hamzanahmed <76833442+hamzanahmed@users.noreply.github.com>
2021-01-01 13:02:26 +0500

    settings.py

--- rl/settings.py
+++ rl/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django-heroku 
+import django_heroku 
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
